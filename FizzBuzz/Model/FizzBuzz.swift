73d1b644aedfc66080a0649fdfb631d4bac7a0d9
Yvette <yvettecook@gmail.com>
2015-10-22 16:32:16 +0100

    FizzBuzz object created. 1 less error - red

---
+++ FizzBuzz/Model/FizzBuzz.swift
@@ -0,0 +1,15 @@
+//
+//  FizzBuzz.swift
+//  FizzBuzz
+//
+//  Created by Yvette Cook on 22/10/2015.
+//  Copyright © 2015 YvetteCook. All rights reserved.
+//
+
+import Foundation
+
+class FizzBuzz : NSObject {
+    
+    
+    
+}
--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -11,9 +11,11 @@ import XCTest
 
 class FizzBuzzTests: XCTestCase {
     
+    var fizzbuzz: FizzBuzz!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        fizzbuzz = FizzBuzz()
     }
     
     override func tearDown() {
@@ -21,16 +23,8 @@ class FizzBuzzTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsDivisibleByThree() {
+        XCTAssertEqual(self.fizzbuzz.isDivisibleByThreee(3), true)
     }
     
 }
